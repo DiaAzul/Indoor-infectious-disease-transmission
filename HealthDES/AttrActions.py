fd4a50cb6050a515f1257e4892436707e54e44bc
David Plummer <28156158+DiaAzul@users.noreply.github.com>
2020-08-21 09:33:00 +0100

    Consolidate common files

--- HealthDES/AttrActions.py
+++ HealthDES/AttrActions.py
@@ -1,20 +1,165 @@
 """ HealthDES - A python library to support discrete event simulation in health and social care """
 
+
+from __future__ import annotations
+from collections.abc import MutableMapping
+from dataclasses import dataclass
 import itertools
 
-from typing import Dict, Any, Callable
+from typing import Any, Callable, Dict, FrozenSet, Iterable, NewType, Optional, Set, Type, Union
 
-from .AttrDict import AttrDict
-from .StateDict import StateDict
+
+# Define valid key and value types for dictionaries (immutable values only)
+AttrDict_KT = str
+AttrDict_VT = NewType('AttrDict_VT', Union[bool,
+                                           bytes,
+                                           str,
+                                           int,
+                                           float,
+                                           complex,
+                                           FrozenSet,
+                                           Optional[bool],
+                                           Optional[bytes],
+                                           Optional[str],
+                                           Optional[int],
+                                           Optional[float],
+                                           Optional[complex],
+                                           Optional[FrozenSet]])
+
+
+class AttrDict(MutableMapping):
+    """Dictionary of immutable attributes."""
+
+    valid_types: Set[Type] = {bool, bytes, str, int, float, complex, frozenset}
+
+    def __init__(self) -> None:
+        self._att_register: Dict[AttrDict_KT, AttrDict_VT] = {}
+
+    def __getitem__(self, key: AttrDict_KT) -> AttrDict_VT:
+        try:
+            return self._att_register[key]
+        except KeyError:
+            print("Attribute {key} does not exist.")
+
+    def __delitem__(self, key: AttrDict_KT) -> None:
+        del self._att_register[key]
+
+    def __setitem__(self, key: AttrDict_KT, value: AttrDict_VT) -> None:
+        if type(value) in AttrDict.valid_types or value is None:
+            self._att_register[key] = value
+        else:
+            raise ValueError(f'Update {key} must be immutable value.')
+
+    def __iter__(self) -> Iterable[AttrDict_KT]:
+        return iter(self._att_register)
+
+    def __len__(self) -> int:
+        return len(self._att_register)
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self._att_register})"
+
+
+@dataclass
+class StateObject:
+    """The StateObject is a classification object with multiple allowable states.
+    + *current_state* is the current state of the object.
+    + *allowable_states* are the states the object is allowed to have.
+    + *default_state* the initial state of the object. Object can be reset to this state.
+    """
+    __slots__ = ['current_state', 'allowable_states', 'default_state']
+    current_state: AttrDict_VT
+    allowable_states: FrozenSet[AttrDict_VT]
+    default_state: AttrDict_VT
+
+
+class StateDict(MutableMapping):
+    """The StateDict is a dictionary of StateObjects holding state attributes for either
+    a person or a resource within the model. Example state objects could include health
+    condition (healthy, unhealthy), injury severity (minor, major, critical), or infection
+    state (susceptible, exposed, infected, recovered).
+    """
+    def __init__(self) -> None:
+        self._status_register: Dict[AttrDict_KT, StateObject] = {}
+
+    def __getitem__(self, key: AttrDict_KT) -> Optional[AttrDict_VT]:
+        try:
+            return self._status_register[key].current_state
+        except KeyError:
+            raise KeyError('{key} does not exist in status register.')
+
+    def __delitem__(self, key: AttrDict_KT) -> None:
+        try:
+            del self._status_register[key]
+        except KeyError:
+            raise KeyError('{key} does not exist in status register.')
+
+    def __setitem__(self, key: AttrDict_KT, value: AttrDict_VT) -> None:
+        try:
+            if value not in self._status_register[key].allowable_states:
+                raise ValueError(f'{value} is not an allowable state in {key} status register.')
+            self._status_register[key].current_state = value
+        except KeyError:
+            raise KeyError('{key} does not exist in status register.')
+
+    def __iter__(self) -> Iterable[AttrDict_KT]:
+        return iter(self._status_register)
+
+    def __len__(self) -> int:
+        return len(self._status_register)
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self._status_register})"
+
+    def add_status_attribute(self, key: AttrDict_KT,
+                             allowable_states: FrozenSet[AttrDict_VT],
+                             default_state: AttrDict_VT):
+        """Add a status attribute to the dictionary.
+
+        Args:
+            key (AttrDict_KT): Name of the state attribute. Used to reference the attribute.
+            allowable_states (FrozenSet[AttrDict_VT]): A frozenset of allowable states for the \
+        attribute. Note, states must be unique within the set.
+            default_state (AttrDict_VT): The default state for the StateObject when it is \
+        initialise or reset.
+
+        Raises:
+            KeyError: Raises a KeyError if the attribute is already defined within the \
+        dictionary
+            ValueError: Raises a value error if the value of default_state is not in the \
+        allowable_states.
+        """
+        if key in self._status_register.keys():
+            raise KeyError('{key} already already exists in status register.')
+        if default_state not in allowable_states:
+            raise ValueError('{default_state} is not an allowable state in {key} status register.')
+        self._status_register[key] = StateObject(current_state=default_state,
+                                                 allowable_states=allowable_states,
+                                                 default_state=default_state)
+
+    def reset(self, key: AttrDict_KT):
+        """Resets the state attribute to the default value.
+
+        Args:
+            key (AttrDict_KT): The name of the state attribute to reset.
+
+        Raises:
+            KeyError: Raises a key error if the state attribute is not in the dictionary.
+        """
+        try:
+            self._status_register[key].current_state = self._status_register[key].default_state
+        except KeyError:
+            raise KeyError('{key} does not exist in status register.')
 
 
 class AttrActions:
     """Inherited by person and resource base classes to provide a standardised interface for recording
-    attributes and methods. Attributes are held in a separately from the class dictionary so that access
-    can be controlled through a standardised interfaces.
+    attributes and methods. Attributes are held in a separately from the class dictionary so that \
+    access can be controlled through a standardised interfaces.
 
     Three types of attributes are available:
-    + *att[name]* - These are regular value attributes, however, they can only store immutable objects.
+    + *att[name]* - These are regular value attributes, however, they can only store immutable \
+    objects.
     + *state[name]* - Attributes that can only hold a defined list of states.
     + *do[name]* - Method attributes, which allow external access to class functions.
 
